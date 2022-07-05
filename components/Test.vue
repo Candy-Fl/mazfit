4ffe9fb27b454f277fbf684353cdeeeaa419eba5
candyy <alekseykim01@gmail.com>
2022-07-05 20:51:14 +0700

    first

---
+++ components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+<h1 class="test-header">Проверка компонентов</h1>
+</template>
+
+<script>
+import {Component, Vue} from "vue-property-decorator";
+
+@Component
+export default class Test extends Vue{
+}
+</script>
+
+<style lang="scss">
+.test-header {
+  font-size: 40px;
+  line-height: 50px;
+  color: red;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <Test></Test>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Test from '~/components/Test.vue';
+import {Component} from "vue-property-decorator";
+
+@Component({
+  components: {
+    Test,
+  },
+
+  head() {
+    return {
+      title: 'mazfit',
+      description: 'mazfit',
+    };
+  }
+})
+
+export default class IndexPage extends Vue{
+}
+</script>
